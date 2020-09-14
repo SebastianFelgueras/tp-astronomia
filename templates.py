b11182a9622a9ef8f0b58afc7e81d70f88134346
Sebastián Felgueras <sebipincha@gmail.com>
2020-09-14 01:43:39 -0300

    medicion 1 y introduccion

--- templates.py
+++ templates.py
@@ -1,7 +1,14 @@
 #los nombres de los archivos y eso puede parecer arcaico pero es para poder usarlo con github pages
 from jinja2 import Environment, FileSystemLoader
 
-renderizar = ["index.html","posicion.html"] #es demasiado obtenerlas automaticamente, igual van a ser 5 archivos, no se justifica
+renderizar = ["index.html","posicion.html","medicion1.html"] #es demasiado obtenerlas automaticamente, igual van a ser 5 archivos, no se justifica
+
+
+with open("textos\\parrafos_index.txt",encoding="utf8") as archivo:
+    output = ""
+    for linea in archivo.read().splitlines():
+        output = f"{output}<p class=\"texto\">{linea}</p>\n"
+    open("textos\\parrafos_index_renderizado.txt",mode="w",encoding="utf8").write(output)
 
 templateLoader = FileSystemLoader(searchpath="./templates")
 templateEnv = Environment(loader=templateLoader)
