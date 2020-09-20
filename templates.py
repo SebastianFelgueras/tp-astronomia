5824fedf1c2f299ff845640b020fe17b8cc350fa
Sebastián Felgueras <sebipincha@gmail.com>
2020-09-20 20:11:56 -0300

    6

--- templates.py
+++ templates.py
@@ -1,7 +1,7 @@
 #los nombres de los archivos y eso puede parecer arcaico pero es para poder usarlo con github pages
 from jinja2 import Environment, FileSystemLoader
 
-renderizar = ["index.html","posicion.html","medicion1.html","medicion2.html"] #es demasiado obtenerlas automaticamente, igual van a ser 5 archivos, no se justifica
+renderizar = ["index.html","posicion.html","medicion1.html","medicion2.html","gnomon.html"] #es demasiado obtenerlas automaticamente, igual van a ser 5 archivos, no se justifica
 
 
 with open("textos\\parrafos_index.txt",encoding="utf8") as archivo:
@@ -14,6 +14,6 @@ templateLoader = FileSystemLoader(searchpath="./templates")
 templateEnv = Environment(loader=templateLoader)
 for template in renderizar:
     outputText = templateEnv.get_template(template).render()  # this is where to put args to the template renderer
-    with open(f"docs\\{template}",mode="w",encoding="utf8") as final:
+    with open(f"{template}",mode="w",encoding="utf8") as final:
         final.write(outputText)
 print("Templates renderizadas")
